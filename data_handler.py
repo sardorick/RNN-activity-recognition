01594d395b76de9c068a6af048fcbb73557dff13
peterzorve <98400766+peterzorve@users.noreply.github.com>
2022-06-17 14:21:19 +0300

    update

--- data_handler.py
+++ data_handler.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 import matplotlib.pyplot as plt 
+from models import Recurrent_Neural_Network_1, Recurrent_Neural_Network_2
 
 
 data_X_path  = "C:/Users/Omistaja/Desktop/epicode/rnn-activity-recognition/Train/X_train.txt"
@@ -79,34 +80,20 @@ def next_stock_batch(batch_size, n_steps, df_base):
 
 
 
-class Recurrent_Neural_Network(nn.Module):
-    def __init__(self, in_size, hidden_size, n_layers, batch_size, seq_length):
-        super(Recurrent_Neural_Network, self).__init__()
-        self.batch_size = batch_size 
-        self.n_layers   = n_layers 
-        self.hidden_size = hidden_size 
-
-        self.rnn = nn.RNN(in_size, hidden_size, n_layers, batch_first=True)
-        self.fully_connected  = nn.Linear(hidden_size, seq_length)
-
-    def forward(self, x):
-        h_0 = torch.zeros((self.n_layers, self.batch_size, self.hidden_size))
-        _, h_n = self.rnn(x, h_0)
-        last_hidden = h_n[-1]
-        output = F.relu(self.fully_connected(last_hidden))
-        return output 
 
 
 
-in_size, hid_size, n_layers, batch_size, seq_length = 18, 10, 15, 50, 32 
+# in_size, hid_size, n_layers, batch_size, seq_length = 18, 20, 25, 64, 100
+# model = Recurrent_Neural_Network_1(in_size, hid_size, n_layers, batch_size, seq_length)
 
-model = Recurrent_Neural_Network(in_size, hid_size, n_layers, batch_size, seq_length)
+in_size, hidden_size, batch_size, num_layers, seq_length = 18, 10, 32, 15, 100
+model = Recurrent_Neural_Network_2(in_size, hidden_size, batch_size, num_layers, seq_length)
 
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.003) 
 
 
-all_train_losses, all_test_losses, epochs = [], [], 1000
+all_train_losses, all_test_losses, epochs = [], [], 50
 
 for epoch in range(epochs):
     train_losses = 0 
@@ -120,7 +107,8 @@ for epoch in range(epochs):
     optimizer.step()
 
     train_losses += loss_train.item()
-    all_train_losses.append(train_losses/len(features_train))
+    average_train_losses = train_losses/len(features_train)
+    all_train_losses.append(average_train_losses)
 
     model.eval()
     with torch.no_grad():
@@ -131,11 +119,12 @@ for epoch in range(epochs):
         prediction_test = model.forward(features_test)
         loss_test = criterion(prediction_test, target_test)
         test_losses += loss_test.item()
-        all_test_losses.append(test_losses/len(features_test))
+        average_test_losses = test_losses/len(features_test)
+        all_test_losses.append(average_test_losses)
 
     model.train()
 
-    print(f'Train loss :    {train_losses:.6f}     |     Test Loss  : {test_losses:.6f}')
+    print(f' {epoch+1:3}/{epochs}  Train loss :    {average_train_losses:.10f}     |     Test Loss  : {average_test_losses:.10f}')
 
 
 plt.plot(all_train_losses, label='train loss') 
---
+++ models.py
@@ -0,0 +1,48 @@
+
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+
+class Recurrent_Neural_Network_1(nn.Module):
+    def __init__(self, in_size, hidden_size, n_layers, batch_size, seq_length):
+        super(Recurrent_Neural_Network_1, self).__init__()
+        self.batch_size = batch_size 
+        self.n_layers   = n_layers 
+        self.hidden_size = hidden_size 
+        self.rnn = nn.RNN(in_size, hidden_size, n_layers, batch_first=True)
+
+
+        self.fully_connected  = nn.Linear(hidden_size, seq_length)
+
+    def forward(self, x):
+        h_0 = torch.zeros((self.n_layers, self.batch_size, self.hidden_size))
+        _, h_n = self.rnn(x, h_0)
+        last_hidden = h_n[-1]
+        output = F.relu(self.fully_connected(last_hidden))
+        return output 
+
+
+
+class Recurrent_Neural_Network_2(nn.Module):
+     def __init__(self, in_size, hidden_size, batch_size, num_layers, seq_len):
+          super().__init__()
+          self.input_size = in_size
+          self.hidden_size = hidden_size
+          self.num_layers = num_layers
+          self.batch_size = batch_size
+          self.seq_len = seq_len
+          self.rnn = nn.RNN(in_size, hidden_size, num_layers, batch_first=True)
+
+          # linear layers
+          # self.fc1 = nn.Linear(self.batch_size*self.hidden_size, 1024)
+          self.fc1 = nn.Linear(self.hidden_size, 1024)
+          self.fc2 = nn.Linear(1024, seq_len)
+
+     def forward(self, x):
+          h_0 = torch.zeros((self.num_layers, self.batch_size, self.hidden_size))
+          rnn_out, h_n = self.rnn(x, h_0)
+          last_hidden = h_n[-1]
+          x = F.relu(last_hidden.flatten())
+          x = F.relu(self.fc1(x))
+          out = self.fc2(x)
+          return out
